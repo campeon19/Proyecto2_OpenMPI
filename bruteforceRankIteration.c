1880c7fb899ca9e557b4bd0ba7124847ce0662a7
Andrei Portales <andreifan123@gmail.com>
2023-05-14 23:54:23 -0600

    Comentarios

--- bruteforceRankIteration.c
+++ bruteforceRankIteration.c
@@ -1,10 +1,16 @@
-//bruteforceNaive.c
-//Tambien cifra un texto cualquiera con un key arbitrario.
-//OJO: asegurarse que la palabra a buscar sea lo suficientemente grande
-//  evitando falsas soluciones ya que sera muy improbable que tal palabra suceda de
-//  forma pseudoaleatoria en el descifrado.
-//>> mpicc bruteforce.c -o desBrute
-//>> mpirun -np <N> desBrute
+/**
+ * Universidad del valle de Guatemala
+ * Computación paralela y distribuida
+ * Proyecto 2
+ * 
+ * Integrantes:
+ *      - Christian Perez 19710
+ *      - Jose Javier Hurtarte 19707
+ *      - Andrei Portales 19825
+ * 
+ * @file bruteforceRankIteration.c
+ * @version 0.1
+ */
 
 #include <string.h>
 #include <stdio.h>
@@ -15,7 +21,12 @@
 #include <stdint.h>
 
 
-
+/**
+ * @brief Lee un archivo y lo guarda en un string
+ * 
+ * @param filename 
+ * @return char* 
+ */
 char *read_file(const char *filename)
 {
     FILE *file = fopen(filename, "r");
@@ -44,7 +55,13 @@ char *read_file(const char *filename)
 }
 
 
-//descifra un texto dado una llave
+/**
+ * @brief Descifra un texto dado una llave
+ * 
+ * @param key Este parametro es la llave que se utilizara para descifrar el texto
+ * @param ciph Este parametro es el texto cifrado que se descifrara
+ * @param len Este parametro es el tamaño del texto cifrado
+ */
 void decrypt(uint64_t key, char *ciph, int len) {
   DES_key_schedule ks;
   DES_cblock k;
@@ -54,7 +71,12 @@ void decrypt(uint64_t key, char *ciph, int len) {
   DES_ecb_encrypt((const_DES_cblock *)ciph, (DES_cblock *)ciph, &ks, DES_DECRYPT);
 }
 
-//cifra un texto dado una llave
+/**
+ * @brief Cifra un texto dado una llave
+ * 
+ * @param key Este parametro es la llave que se utilizara para cifrar el texto
+ * @param ciph Este parametro es el texto que se cifrara
+ */
 void encrypt(uint64_t key, char *ciph) {
   DES_key_schedule ks;
   DES_cblock k;
@@ -66,6 +88,15 @@ void encrypt(uint64_t key, char *ciph) {
 
 //palabra clave a buscar en texto descifrado para determinar si se rompio el codigo
 char search[] = "es una prueba de";
+
+/**
+ * @brief Intenta descifrar un texto dado una llave y busca un texto en el texto descifrado
+ * 
+ * @param key   Este parametro es la llave que se utilizara para descifrar el texto
+ * @param ciph Este parametro es el texto cifrado que se descifrara
+ * @param len Este parametro es el tamaño del texto cifrado
+ * @return int  1 si se encontro el texto, 0 si no se encontro
+ */
 int tryKey(uint64_t key, char *ciph, int len){	
   char temp[len+1]; //+1 por el caracter terminal
   memcpy(temp, ciph, len);
@@ -74,11 +105,8 @@ int tryKey(uint64_t key, char *ciph, int len){
   return strstr((char *)temp, search) != NULL;
 }
 
-char eltexto[] = "Esta es una prueba de proyecto 2";
+char eltexto[] = "Esta es una prueba de proyecto 2"; //texto a cifrar
 uint64_t the_key = 12L;
-//2^56 / 4 es exactamente 18014398509481983
-//uint64_t the_key = 18014398509481983L;
-//long the_key = 18014398509481983L +1L;
 int main(int argc, char *argv[]) {
   if (argc != 3)
   {
@@ -86,8 +114,8 @@ int main(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   }
 
-  const char *filename = argv[1];
-  long the_key = strtol(argv[2], NULL, 10);
+  const char *filename = argv[1]; // Nombre del archivo
+  long the_key = strtol(argv[2], NULL, 10); // Llave privada
 
   int N, id;
   uint64_t upper = (1ULL << 56); // upper bound DES keys 2^56
@@ -95,20 +123,20 @@ int main(int argc, char *argv[]) {
   MPI_Request req;
   double start_time, end_time; // Declare the variables to hold the start and end times
 
-  char *eltexto = read_file(filename);
+  char *eltexto = read_file(filename); // Lee el archivo que contiene el texto
   int ciphlen = strlen(eltexto);
-  MPI_Comm comm = MPI_COMM_WORLD;
+  MPI_Comm comm = MPI_COMM_WORLD; // Communicator
 
   // Encrypt the text
-  char cipher[ciphlen + 1];
-  memcpy(cipher, eltexto, ciphlen);
-  cipher[ciphlen] = 0;
-  encrypt(the_key, cipher);
+  char cipher[ciphlen + 1]; //+1 por el caracter terminal
+  memcpy(cipher, eltexto, ciphlen);  // copia el texto cifrado
+  cipher[ciphlen] = 0; // caracter terminal
+  encrypt(the_key, cipher); // cifra el texto
 
   // Initialize MPI
-  MPI_Init(NULL, NULL);
-  MPI_Comm_size(comm, &N);
-  MPI_Comm_rank(comm, &id);
+  MPI_Init(NULL, NULL); // inicializa MPI
+  MPI_Comm_size(comm, &N);  // obtiene el numero de procesos
+  MPI_Comm_rank(comm, &id);// obtiene el id del proceso
 
   uint64_t found = 0;
   int ready = 0;
@@ -116,20 +144,21 @@ int main(int argc, char *argv[]) {
   printf("Process %d starting\n", id);
 
   
-  MPI_Irecv(&found, 1, MPI_LONG, MPI_ANY_SOURCE, MPI_ANY_TAG, comm, &req);
+  MPI_Irecv(&found, 1, MPI_LONG, MPI_ANY_SOURCE, MPI_ANY_TAG, comm, &req); // recibe el mensaje de cualquier proceso
 
   start_time = MPI_Wtime(); // tiempo inicial del calculo
   for (uint64_t i = id; i < upper; i+=N) {
-    MPI_Test(&req, &ready, MPI_STATUS_IGNORE);
+    MPI_Test(&req, &ready, MPI_STATUS_IGNORE); // prueba si el mensaje ha llegado
     if (ready)
       break;
 
-    if (tryKey(i, cipher, ciphlen)) {
+    if (tryKey(i, cipher, ciphlen)) // Verificando si logra desifrar el texto con una llave dada
+    {
       
       found = i;
       printf("Process %d found the key\n", id);
       for (int node = 0; node < N; node++) {
-        MPI_Send(&found, 1, MPI_LONG, node, 0, comm);
+        MPI_Send(&found, 1, MPI_LONG, node, 0, comm); // envia el mensaje a todos los procesos
       }
       break;
     }
--- bruteforceWorkStealing.c
+++ bruteforceWorkStealing.c
@@ -1,3 +1,18 @@
+/**
+ * Universidad del valle de Guatemala
+ * Computación paralela y distribuida
+ * Proyecto 2
+ * 
+ * Integrantes:
+ *      - Christian Perez 19710
+ *      - Jose Javier Hurtarte 19707
+ *      - Andrei Portales 19825
+ * 
+ * @file bruteforceWorkStealing.c
+ * @version 0.1
+ */
+
+
 #include <mpi.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -6,6 +21,12 @@
 #include <unistd.h>
 #include <openssl/des.h>
 
+/**
+ * @brief Lee un archivo y lo guarda en un string
+ * 
+ * @param filename 
+ * @return char* 
+ */
 char *read_file(const char *filename)
 {
     FILE *file = fopen(filename, "r");
@@ -33,18 +54,29 @@ char *read_file(const char *filename)
     return file_contents;
 }
 
-// descifra un texto dado una llave
+/**
+ * @brief Descifra un texto dado una llave
+ * 
+ * @param key Este parametro es la llave que se utilizara para descifrar el texto
+ * @param ciph Este parametro es el texto cifrado que se descifrara
+ * @param len Este parametro es el tamaño del texto cifrado
+ */
 void decrypt(uint64_t key, char *ciph, int len)
 {
     DES_key_schedule ks;
     DES_cblock k;
-    memcpy(k, &key, 8);
-    DES_set_odd_parity(&k);
-    DES_set_key_checked(&k, &ks);
-    DES_ecb_encrypt((const_DES_cblock *)ciph, (DES_cblock *)ciph, &ks, DES_DECRYPT);
+    memcpy(k, &key, 8); // copia la llave en el bloque k
+    DES_set_odd_parity(&k); // establece la paridad impar
+    DES_set_key_checked(&k, &ks); // establece la llave
+    DES_ecb_encrypt((const_DES_cblock *)ciph, (DES_cblock *)ciph, &ks, DES_DECRYPT); // descifra el texto
 }
 
-// cifra un texto dado una llave
+/**
+ * @brief Cifra un texto dado una llave
+ * 
+ * @param key Este parametro es la llave que se utilizara para cifrar el texto
+ * @param ciph Este parametro es el texto que se cifrara
+ */
 void encrypt(uint64_t key, char *ciph)
 {
     DES_key_schedule ks;
@@ -55,8 +87,17 @@ void encrypt(uint64_t key, char *ciph)
     DES_ecb_encrypt((const_DES_cblock *)ciph, (DES_cblock *)ciph, &ks, DES_ENCRYPT);
 }
 
-// palabra clave a buscar en texto descifrado para determinar si se rompio el codigo
-char search[] = "es una prueba de";
+//palabra clave a buscar en texto descifrado para determinar si se rompio el codigo
+char search[] = "es una prueba de"; // texto a buscar en el texto desifrado
+
+/**
+ * @brief Intenta descifrar un texto dado una llave y busca un texto en el texto descifrado
+ * 
+ * @param key   Este parametro es la llave que se utilizara para descifrar el texto
+ * @param ciph Este parametro es el texto cifrado que se descifrara
+ * @param len Este parametro es el tamaño del texto cifrado
+ * @return int  1 si se encontro el texto, 0 si no se encontro
+ */
 int tryKey(uint64_t key, char *ciph, int len)
 {
     char temp[len + 1]; //+1 por el caracter terminal
@@ -66,6 +107,13 @@ int tryKey(uint64_t key, char *ciph, int len)
     return strstr((char *)temp, search) != NULL;
 }
 
+/**
+ * @brief Envia un mensaje a un proceso, Esta funcion se encarga de pedir trabajo a un proceso
+ * 
+ * @param work_source 
+ * @param comm 
+ * @return int 
+ */
 int request_work(int *work_source, MPI_Comm comm)
 {
     MPI_Status status;
@@ -75,21 +123,22 @@ int request_work(int *work_source, MPI_Comm comm)
     return work_received;
 }
 
+
 int main(int argc, char **argv)
 {
     if (argc != 3)
     {
-        fprintf(stderr, "Uso: %s <nombre_archivo> <llave_privada>\n", argv[0]);
-        exit(EXIT_FAILURE);
+        fprintf(stderr, "Uso: %s <nombre_archivo> <llave_privada>\n", argv[0]); // nombre del archivo y llave privada
+        exit(EXIT_FAILURE); // termina el programa
     }
 
-    const char *filename = argv[1];
-    long the_key = strtol(argv[2], NULL, 10);
-    double start_time, end_time;
+    const char *filename = argv[1]; // nombre del archivo
+    long the_key = strtol(argv[2], NULL, 10); // la llave que se recibe como parametro
+    double start_time, end_time; // tiempo de inicio y fin
 
-    int N, id;
+    int N, id; // numero de procesos y id del proceso
     uint64_t upper = (1ULL << 56); // upper bound DES keys 2^56
-    uint64_t mylower, myupper;
+    uint64_t mylower, myupper; 
     MPI_Status st;
     MPI_Request req;
 
@@ -99,42 +148,42 @@ int main(int argc, char **argv)
     MPI_Comm comm = MPI_COMM_WORLD;
 
     // cifrar el texto
-    char cipher[ciphlen + 1];
-    memcpy(cipher, eltexto, ciphlen);
-    cipher[ciphlen] = 0;
-    encrypt(the_key, cipher);
+    char cipher[ciphlen + 1]; //+1 por el caracter terminal
+    memcpy(cipher, eltexto, ciphlen); // copia el texto cifrado
+    cipher[ciphlen] = 0; // caracter terminal
+    encrypt(the_key, cipher); // cifra el texto
 
-    MPI_Init(NULL, NULL);
-    MPI_Comm_size(comm, &N);
-    MPI_Comm_rank(comm, &id);
-    start_time = MPI_Wtime();
+    MPI_Init(NULL, NULL); // inicializa MPI
+    MPI_Comm_size(comm, &N); // obtiene el numero de procesos
+    MPI_Comm_rank(comm, &id); // obtiene el id del proceso
+    start_time = MPI_Wtime(); // tiempo de inicio
 
     // Configurar work stealing
-    int chunk_size = 1000;
-    long work_counter = id * chunk_size;
-    long max_work = upper / N * (id + 1);
-    int work_source;
+    int chunk_size = 1000; // tamaño del chunk
+    long work_counter = id * chunk_size; // contador de trabajo
+    long max_work = upper / N * (id + 1); // maximo de trabajo
+    int work_source; // fuente de trabajo
 
-    long found = 0L;
+    long found = 0L; 
     int ready = 0;
-    MPI_Irecv(&found, 1, MPI_LONG, MPI_ANY_SOURCE, MPI_ANY_TAG, comm, &req);
+    MPI_Irecv(&found, 1, MPI_LONG, MPI_ANY_SOURCE, MPI_ANY_TAG, comm, &req); // recibe el trabajo
 
-    while (!ready && work_counter < max_work)
+    while (!ready && work_counter < max_work) // mientras no este listo y el contador de trabajo sea menor al maximo de trabajo
     {
-        for (long i = work_counter; i < work_counter + chunk_size; ++i)
+        for (long i = work_counter; i < work_counter + chunk_size; ++i) // recorre el contador de trabajo
         {
             MPI_Test(&req, &ready, MPI_STATUS_IGNORE);
             if (ready)
                 break;
 
-            if (tryKey(i, cipher, ciphlen))
+            if (tryKey(i, cipher, ciphlen)) // Verificando si logra desifrar el texto con una llave dada
             {
                 found = i;
                 end_time = MPI_Wtime();  // tiempo de finalizacion
                 printf("Process %d found the key\n", id);
                 for (int node = 0; node < N; node++)
                 {
-                    MPI_Send(&found, 1, MPI_LONG, node, 0, comm);
+                    MPI_Send(&found, 1, MPI_LONG, node, 0, comm); // Envia el trabajo al nodo que lo necesita
                 }
                 break;
             }
@@ -145,8 +194,8 @@ int main(int argc, char **argv)
             work_counter += chunk_size;
             if (work_counter >= max_work)
             {
-                work_counter = request_work(&work_source, comm);
-                max_work = work_counter + chunk_size;
+                work_counter = request_work(&work_source, comm); // pide trabajo a un proceso
+                max_work = work_counter + chunk_size; // actualiza el maximo de trabajo
             }
         }
     }
@@ -154,8 +203,8 @@ int main(int argc, char **argv)
     // wait y luego imprimir el texto
     if (id == 0)
     {
-        MPI_Wait(&req, &st);
-        decrypt(found, cipher, ciphlen);
+        MPI_Wait(&req, &st); // espera a que el trabajo se complete
+        decrypt(found, cipher, ciphlen); // desifra el texto
         printf("Key = %li\n\n", found);
         printf("%s\n", cipher);
     }
@@ -164,7 +213,7 @@ int main(int argc, char **argv)
     double elapsed_time = end_time - start_time;
 
     double max_elapsed_time;
-    MPI_Reduce(&elapsed_time, &max_elapsed_time, 1, MPI_DOUBLE, MPI_MAX, 0, comm);
+    MPI_Reduce(&elapsed_time, &max_elapsed_time, 1, MPI_DOUBLE, MPI_MAX, 0, comm); // obtiene el tiempo maximo de ejecucion
 
     if (id == 0)
     {
